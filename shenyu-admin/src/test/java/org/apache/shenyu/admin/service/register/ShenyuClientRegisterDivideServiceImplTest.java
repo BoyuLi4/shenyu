2c397d92bf25f06b142199fb07254b9b5ec8c6a5
BoyuLi4 <112122577+BoyuLi4@users.noreply.github.com>
2022-10-03 22:51:32 -0500

    Update ShenyuClientRegisterDivideServiceImplTest.java

--- shenyu-admin/src/test/java/org/apache/shenyu/admin/service/register/ShenyuClientRegisterDivideServiceImplTest.java
+++ shenyu-admin/src/test/java/org/apache/shenyu/admin/service/register/ShenyuClientRegisterDivideServiceImplTest.java
@@ -44,6 +44,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -116,7 +117,8 @@ public final class ShenyuClientRegisterDivideServiceImplTest {
         when(selectorDO.getHandle()).thenReturn(returnStr);
         doReturn(false).when(shenyuClientRegisterDivideService).doSubmit(any(), any());
         String actual = shenyuClientRegisterDivideService.buildHandle(list, selectorDO);
-        assertEquals(expected.replaceAll("\\d{13}", "0"), actual.replaceAll("\\d{13}", "0"));
+        //assertEquals(expected.replaceAll("\\d{13}", "0"), actual.replaceAll("\\d{13}", "0"));
+        assertEquals(orderdResult(expected.replaceAll("\\d{13}", "0")), orderdResult(actual.replaceAll("\\d{13}", "0")));
         List<DivideUpstream> resultList = GsonUtils.getInstance().fromCurrentList(actual, DivideUpstream.class);
         assertEquals(resultList.size(), 3);
         assertEquals(resultList.stream().filter(r -> list.stream().map(dto -> CommonUpstreamUtils.buildUrl(dto.getHost(), dto.getPort()))
@@ -184,4 +186,28 @@ public final class ShenyuClientRegisterDivideServiceImplTest {
             throw new ShenyuException(e.getCause());
         }
     }
+
+    private String orderdResult(final String result) {
+        ArrayList<String> list = new ArrayList<String>();
+        String[] splitStr = result.split("}");
+        String out = "";
+        for (String str: splitStr) {
+            String newStr = str.replaceAll("[\\}\\{\\[\\]]", "");
+            if (newStr.length() > 3) {
+                String[] small = newStr.split(",");
+                for (String str2:small) {
+                    if (str2.length() > 3) {
+                        list.add(str2);
+                    }                  
+                }
+            }
+            list.sort(Comparator.naturalOrder());
+            out += list.toString();
+            list.clear();
+            
+        }
+        return out;
+        
+    }
+    
 }
